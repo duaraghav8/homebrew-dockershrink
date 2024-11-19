581ed35089f661a2f82e9590f87a2b96b5177d8c
Raghav Dua <duaraghav8@protonmail.com>
2024-11-19 12:11:33 +0100

    Brew formula update for dockershrink version v0.1.0

--- dockershrink.rb
+++ dockershrink.rb
@@ -12,7 +12,7 @@ class Dockershrink < Formula
   on_macos do
     on_intel do
       url "https://github.com/duaraghav8/dockershrink-cli/releases/download/v0.1.0/dockershrink_0.1.0_darwin_amd64.tar.gz"
-      sha256 "23758508c5d532104c60df89d0564f38bd33b5021241b7cd52ebf21a225f88e0"
+      sha256 "5237c5549c1213c6f2d762b79e458f42a4d17ef352c944a04759f31108464f4d"
 
       def install
         bin.install "dockershrink"
@@ -20,7 +20,7 @@ class Dockershrink < Formula
     end
     on_arm do
       url "https://github.com/duaraghav8/dockershrink-cli/releases/download/v0.1.0/dockershrink_0.1.0_darwin_arm64.tar.gz"
-      sha256 "176282702f57c4f108f040a1b118752707efb7d0ef6f80a24c4b846ce0697419"
+      sha256 "f4437cd4ab764ace96a88511f3e6cacb03034e60411bbf7165d7b3f90adcd940"
 
       def install
         bin.install "dockershrink"
@@ -32,7 +32,7 @@ class Dockershrink < Formula
     on_intel do
       if Hardware::CPU.is_64_bit?
         url "https://github.com/duaraghav8/dockershrink-cli/releases/download/v0.1.0/dockershrink_0.1.0_linux_amd64.tar.gz"
-        sha256 "a3edc985adce455c497fd020c866a4bc53bd90fe58622b36f3d1deb32677d4eb"
+        sha256 "81907da7f9968f3b331ee01bc50ff1d4da19850c625f9fd165caf7f63a85b750"
 
         def install
           bin.install "dockershrink"
@@ -42,7 +42,7 @@ class Dockershrink < Formula
     on_arm do
       if Hardware::CPU.is_64_bit?
         url "https://github.com/duaraghav8/dockershrink-cli/releases/download/v0.1.0/dockershrink_0.1.0_linux_arm64.tar.gz"
-        sha256 "fc5e0c3892ef34aba9553a20b29aeb48eeb24eeeaaabc5de89ef357905b3c850"
+        sha256 "fb7ed38152dcfad860fdbcc8a2a0bd8e49fcd96b1f194ef6bef0f26224c87bea"
 
         def install
           bin.install "dockershrink"
